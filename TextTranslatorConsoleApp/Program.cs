d48c89938447b1c85c69230900b7f93b5b0a66c2
enginunal <enginunal@gmail.com>
2017-10-04 15:56:52 +0300

    Add project files.

---
+++ TextTranslatorConsoleApp/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace TextTranslatorConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string ApiKey = "6c965416747744d8b13103450329f364";
+            string input = "Bugün hava güneşli.";
+            string targetLang = "en";
+
+
+            Task.Run(async () =>
+            {
+                var accessToken = await GetAuthenticationToken(ApiKey);
+                var output = await TranslateText(input, targetLang, accessToken);
+                Console.WriteLine(output);
+                                
+            }).Wait();
+
+        }
+
+        static async Task<string> TranslateText(string inputText, string language, string accessToken)
+        {
+            string url = "http://api.microsofttranslator.com/v2/Http.svc/Translate";
+            string query = $"?text={System.Net.WebUtility.UrlEncode(inputText)}&to={language}&contentType=text/plain";
+
+            using (var client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", accessToken);
+                var response = await client.GetAsync(url + query);
+                var result = await response.Content.ReadAsStringAsync();
+
+                if (!response.IsSuccessStatusCode)
+                    return "Hata: " + result;
+
+                var translatedText = XElement.Parse(result).Value;
+                return translatedText;
+            }
+        }
+
+        static async Task<string> GetAuthenticationToken(string key)
+        {
+            string endpoint = "https://api.cognitive.microsoft.com/sts/v1.0/issueToken";
+
+            using (var client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Key", key);
+                var response = await client.PostAsync(endpoint, null);
+                var token = await response.Content.ReadAsStringAsync();
+                return token;
+            }
+        }
+    }
+}
